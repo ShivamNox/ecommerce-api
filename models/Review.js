34fefb2ccf606c9ba948fde3ce440a33995b3ca1
Shivam Kumar <shivamnox0@gmail.com>
2025-10-22 17:24:27 +0530

    Create Review.js

---
+++ models/Review.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  comment: {
+    type: String,
+    required: true,
+    maxlength: 500
+  }
+}, {
+  timestamps: true
+});
+
+reviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+reviewSchema.statics.calculateAverageRating = async function(productId) {
+  const stats = await this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        _id: '$product',
+        avgRating: { $avg: '$rating' },
+        numReviews: { $sum: 1 }
+      }
+    }
+  ]);
+
+  if (stats.length > 0) {
+    await mongoose.model('Product').findByIdAndUpdate(productId, {
+      rating: Math.round(stats[0].avgRating * 10) / 10,
+      numReviews: stats[0].numReviews
+    });
+  } else {
+    await mongoose.model('Product').findByIdAndUpdate(productId, {
+      rating: 0,
+      numReviews: 0
+    });
+  }
+};
+
+reviewSchema.post('save', function() {
+  this.constructor.calculateAverageRating(this.product);
+});
+
+reviewSchema.post('remove', function() {
+  this.constructor.calculateAverageRating(this.product);
+});
+
+module.exports = mongoose.model('Review', reviewSchema);
